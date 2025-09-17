ba99855b771238b3c435a760ad2d8680d5e818dd
AndreasBagasgoro <andreasbagasgoro04@gmail.com>
2025-09-17 08:31:51 +0700

    style(HeroSection): Adjust size

--- src/components/NavbarGlass/NavbarGlass.tsx
+++ src/components/NavbarGlass/NavbarGlass.tsx
@@ -51,7 +51,7 @@ const NavbarGlass: React.FC = () => {
 
   return (
     <header className="fixed top-0 left-0 w-full z-50 px-4 sm:px-6 md:px-8 lg:px-[17.184vh] py-[0.716vh] bg-transparent">
-      <nav className="max-w-[1440px] mx-auto flex items-center justify-between">
+      <nav className="max-w-[257.755vh ] mx-auto flex items-center justify-between">
         {/* Logo */}
         <a href="#beranda" aria-label="Logo IKAPEMA KEPRI—MALANG">
           <img
