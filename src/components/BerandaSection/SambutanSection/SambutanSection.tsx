4ffefe36e0c3cb17469ccfeef51a78685b138ba2
AndreasBagasgoro <andreasbagasgoro04@gmail.com>
2025-10-28 21:57:03 +0700

    style(SambutanSection): Adding conditional rendering on header

--- src/components/BerandaSection/SambutanSection/SambutanSection.tsx
+++ src/components/BerandaSection/SambutanSection/SambutanSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { memo } from "react";
+import React, { memo, useState, useEffect } from "react";
 import Image from "next/image";
 import { useSambutanAnimation } from "./hooks/useSambutanAnimation";
 import KetumProfile from "./components/KetumProfile";
@@ -9,13 +9,31 @@ import SambutanText from "./components/SambutanText";
 const SambutanSection: React.FC = () => {
   // Memanggil custom hook untuk mendapatkan semua state dan logika animasi
   const { sectionRef, isSectionInView, headerClasses } = useSambutanAnimation();
+  
+  // State untuk mendeteksi iOS
+  const [isIOS, setIsIOS] = useState(false);
+
+  // Deteksi iOS device
+  useEffect(() => {
+    const detectIOS = () => {
+      return /iPad|iPhone|iPod/.test(navigator.userAgent) || 
+             (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+    };
+    
+    setIsIOS(detectIOS());
+  }, []);
+
+  // Tentukan path gambar berdasarkan device
+  const headingImageSrc = isIOS 
+    ? "/heading/HeadingSambutan.webp" 
+    : "/heading/HeadingSambutan.svg";
 
   return (
     <section ref={sectionRef} id="sambutan" className="py-10 md:py-16 lg:py-[15.815vh]">
       <div className="flex flex-col items-center px-12 sm:px-12 md:px-12 lg:px-[10.544vh] xl:px-[21.087vh]">
         <div className={headerClasses}>
           <Image
-            src="/heading/HeadingSambutan.svg"
+            src={headingImageSrc}
             alt="Heading Sambutan"
             width={454}
             height={100}
