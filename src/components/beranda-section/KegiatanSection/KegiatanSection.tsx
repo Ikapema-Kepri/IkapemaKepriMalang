e73f443089a57dcb8e5c6883d14dccabf094e771
AndreasBagasgoro <andreasbagasgoro04@gmail.com>
2026-01-26 10:39:41 +0700

    fix(Global): Fixing build error

--- src/components/beranda-section/KegiatanSection/KegiatanSection.tsx
+++ src/components/beranda-section/KegiatanSection/KegiatanSection.tsx
@@ -4,7 +4,8 @@ import React, { memo } from "react";
 
 import { useKegiatanAnimation } from "./hooks/useKegiatanAnimation";
 import KegiatanHeader from "./components/KegiatanHeader";
-import Slider from "../../UI/slider";
+import Slider from "@/components/UI/slider";
+
 
 
 const KegiatanSection: React.FC = () => {
